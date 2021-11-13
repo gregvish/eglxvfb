ce62b74cd6e2d01eba3fe093b74272170d15cded
Greg Vish <gregoryvish@gmail.com>
2021-11-13 12:10:57 +0200

    some fixes

--- local-test.sh
+++ local-test.sh
@@ -12,11 +12,12 @@ Xvfb -screen 0 1920x1080x24+32 +extension GLX +render -noreset -nocursor \
      -fbdir $XVFB_DIR $XVFB_DISPLAY &
 sleep 1
 
+DISPLAY=$XVFB_DISPLAY xrdb -merge ../Xresources
 DISPLAY=$XVFB_DISPLAY socat unix-l:$XVFB_DIR/Xdamage,fork exec:./Xdamage &
 DISPLAY=$XVFB_DISPLAY socat unix-l:$XVFB_DIR/Xtest,fork exec:./Xtest &
-DISPLAY=$XVFB_DISPLAY icewm &
 sleep 1
-DISPLAY=$XVFB_DISPLAY vglrun -v -d :0 glxgears -info &
 
+DISPLAY=$XVFB_DISPLAY i3 &
+DISPLAY=$XVFB_DISPLAY xterm &
 DISPLAY=:0 ./XEGLXvfb $XVFB_DIR
 
