9b865e6946cfcb904824b89332e9b4525675f84a
gregvish <gregoryvish@gmail.com>
2022-07-19 19:01:24 +0000

    script for running on yalcut setup with mesa

--- run.sh
+++ run.sh
@@ -8,24 +8,27 @@ XVFB_DISPLAY=":0"
 
 BUILD_DIR=$(dirname "${BASH_SOURCE[0]}")/build
 
-
 export XDG_RUNTIME_DIR=/run/user
-export LD_LIBRARY_PATH=/opt/mesa/lib/aarch64-linux-gnu/
-export VK_ICD_FILENAMES=/opt/mesa/share/vulkan/icd.d/freedreno_icd.aarch64.json
-export GALLIUM_DRIVER=zink
-export MESA_LOADER_DRIVER_OVERRIDE=zink
 
 
 mkdir -p $XVFB_DIR
-Xvfb -screen 0 2160x1080x24+32 +extension GLX +render -noreset -nocursor \
+Xvfb -screen 0 2160x1080x24+32 +render +extension GLX -noreset -nocursor \
      -fbdir $XVFB_DIR $XVFB_DISPLAY &
 sleep 1
 export DISPLAY=$XVFB_DISPLAY
-
 
 $BUILD_DIR/fd_server $XVFB_DIR/Xvfb_screen0 $ABSTRACT_DIR/Xvfb_shm &
 
 
 xrdb -merge $BUILD_DIR/../Xresources
-TERMINAL=kitty i3
 
+
+export $(dbus-launch)
+xfwm4 --sm-client-disable &
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mesa/lib/aarch64-linux-gnu/
+export VK_ICD_FILENAMES=/opt/mesa/share/vulkan/icd.d/freedreno_icd.aarch64.json
+export GALLIUM_DRIVER=zink
+export MESA_LOADER_DRIVER_OVERRIDE=zink
+
+bash
